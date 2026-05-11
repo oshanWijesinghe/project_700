c05425bae3de77b9515b0ecf5b2d0b52c2ba2a1f
oshanWijesinghe <w2052044@westminster.ac.uk>
2026-05-11 14:12:50 +0530

    created vpc and subnets

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_vpc" "my_test_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block = var.cidr
 
   tags = {
-    Name = "Oshan-Test-VPC"
+    Name = "terraform_vpc"
+  }
+}
+
+resource "aws_subnet" "subnet_1" {
+
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "terraform_subnet_1"
+  }
+}
+
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "terraform_subnet_2"
   }
 }
