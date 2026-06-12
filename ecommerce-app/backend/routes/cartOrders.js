50c8371da2e05617c6c2c207bbf64b5ba10818d1
oshanWijesinghe <w2052044@westminster.ac.uk>
2026-06-12 11:26:07 +0530

    update cart oders

--- ecommerce-app/backend/routes/cartOrders.js
+++ ecommerce-app/backend/routes/cartOrders.js
@@ -13,7 +13,7 @@ cartRouter.put('/:id', updateCartItem);
 cartRouter.delete('/clear', clearCart);
 cartRouter.delete('/:id', removeFromCart);
  
-// All order routes require authentication
+// All order routes requirdsde authentication
 orderRouter.use(authenticate);
 orderRouter.get('/', getOrders);
 orderRouter.get('/:id', getOrder);
