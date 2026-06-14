676eefa7165062ed21fe1175dbffe2118d019e5e
oshanWijesinghe <w2052044@westminster.ac.uk>
2026-06-14 07:17:48 +0530

    update variables.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,9 @@ resource "aws_vpc" "terraform_vpc" {
 
 # 2 public subnets in different availability zones
 resource "aws_subnet" "subnet_1" {
-
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = var.subnet_1_cidr
-  availability_zone       = "us-east-1a"
+  availability_zone       = var.az_1
   map_public_ip_on_launch = true
 
   tags = {
@@ -22,7 +21,7 @@ resource "aws_subnet" "subnet_1" {
 resource "aws_subnet" "subnet_2" {
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = var.subnet_2_cidr
-  availability_zone       = "us-east-1b"
+  availability_zone       = var.az_2
   map_public_ip_on_launch = true
 
   tags = {
@@ -30,9 +29,7 @@ resource "aws_subnet" "subnet_2" {
   }
 }
 
-
-
-#internet gateway for the VPC
+# Internet gateway for the VPC
 resource "aws_internet_gateway" "terraform_igw" {
   vpc_id = aws_vpc.terraform_vpc.id
 
@@ -41,14 +38,13 @@ resource "aws_internet_gateway" "terraform_igw" {
   }
 }
 
-
-#route table for public subnets
+# Route table for public subnets
 resource "aws_route_table" "terraform_route_table" {
   vpc_id = aws_vpc.terraform_vpc.id
 
-  #where  to send traffic  ,how to send traffic
+  # Where to send traffic, how to send traffic
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.open_internet_cidr
     gateway_id = aws_internet_gateway.terraform_igw.id
   }
 
@@ -57,25 +53,18 @@ resource "aws_route_table" "terraform_route_table" {
   }
 }
 
-#route table associations for public subnets
-
-resource "aws_route_table_association" "internet_to_public_subnets_2" {
-  subnet_id      = aws_subnet.subnet_2.id
-  route_table_id = aws_route_table.terraform_route_table.id
-
-
-}
-
+# Route table associations for public subnets
 resource "aws_route_table_association" "internet_to_public_subnets_1" {
   subnet_id      = aws_subnet.subnet_1.id
   route_table_id = aws_route_table.terraform_route_table.id
 }
 
+resource "aws_route_table_association" "internet_to_public_subnets_2" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.terraform_route_table.id
+}
 
-
-
-#security group for ALB and EC2 instances
-
+# Security group for ALB and EC2 instances
 resource "aws_security_group" "terraform_sg" {
   description = "Allow security to ALB and EC2 instances"
   vpc_id      = aws_vpc.terraform_vpc.id
@@ -85,64 +74,56 @@ resource "aws_security_group" "terraform_sg" {
   }
 }
 
-#ingress rule to allow HTTP traffic from the VPC CIDR block to the security group
+# Ingress rule to allow HTTP traffic
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.terraform_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  #port rage
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+  cidr_ipv4         = var.open_internet_cidr
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
 }
 
-#ingress rule to allow SSH traffic from the VPC CIDR block to the security group
+# Ingress rule to allow SSH traffic
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.terraform_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.open_internet_cidr
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 
-
-
-#egress rule to allow all outbound traffic from the security group to the VPC CIDR block 
+# Egress rule to allow all outbound traffic
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.terraform_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.open_internet_cidr
   ip_protocol       = "-1"
 }
 
-
-
-
-
-
+# Web Server 1
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-091138d0f0d41ff90"
-  instance_type          = "t3.micro"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.terraform_sg.id]
   subnet_id              = aws_subnet.subnet_1.id
-
 
   tags = {
     Name = "webserver1"
   }
 }
 
+# Web Server 2
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-091138d0f0d41ff90"
-  instance_type          = "t3.micro"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.terraform_sg.id]
   subnet_id              = aws_subnet.subnet_2.id
-
 
   tags = {
     Name = "webserver2"
   }
 }
 
-#aws_lb for the web servers
+# AWS ALB for the web servers
 resource "aws_lb" "terraform_alb" {
   name               = "terraform-alb"
   internal           = false
@@ -152,15 +133,12 @@ resource "aws_lb" "terraform_alb" {
 
   enable_deletion_protection = true
 
-
-
   tags = {
     Name = "terraform-alb"
   }
 }
 
-
-#aws target group for the web servers
+# AWS Target Group for the web servers
 resource "aws_lb_target_group" "terraform_target_group" {
   name     = "terraform-target-group"
   port     = 80
@@ -181,24 +159,21 @@ resource "aws_lb_target_group" "terraform_target_group" {
   }
 }
 
-
-#attach the web servers1 to the target group
+# Attach Web Server 1 to the Target Group
 resource "aws_lb_target_group_attachment" "webserver1_attachment" {
   target_group_arn = aws_lb_target_group.terraform_target_group.arn
   target_id        = aws_instance.webserver1.id
   port             = 80
 }
 
-#attach the web servers2 to the target group
+# Attach Web Server 2 to the Target Group
 resource "aws_lb_target_group_attachment" "webserver2_attachment" {
   target_group_arn = aws_lb_target_group.terraform_target_group.arn
   target_id        = aws_instance.webserver2.id
   port             = 80
 }
 
-
-
-#aws_lb_listener for the ALB
+# AWS ALB Listener
 resource "aws_lb_listener" "terraform_listener" {
   load_balancer_arn = aws_lb.terraform_alb.arn
   port              = 80
@@ -208,9 +183,4 @@ resource "aws_lb_listener" "terraform_listener" {
     target_group_arn = aws_lb_target_group.terraform_target_group.arn
     type             = "forward"
   }
-}
-
-
-output "loadbalancerdns" {
-  value = aws_lb.terraform_alb.dns_name
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,47 @@
 variable "cidr" {
-  default = "10.0.0.0/16"
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_1_cidr" {
-  default = "10.0.1.0/24"
+  description = "The CIDR block for public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "subnet_2_cidr" {
-  default = "10.0.2.0/24"
+  description = "The CIDR block for public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "az_1" {
+  description = "Availability Zone for subnet 1"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "az_2" {
+  description = "Availability Zone for subnet 2"
+  type        = string
+  default     = "us-east-1b"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the web server instances"
+  type        = string
+  default     = "ami-091138d0f0d41ff90"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "open_internet_cidr" {
+  description = "CIDR block representing the open internet"
+  type        = string
+  default     = "0.0.0.0/0"
 }
